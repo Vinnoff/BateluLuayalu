1437f1ab2a48d73a2ddf1f1ca3e96fed31386299
Vincent Offroy <offroy.vincent@gmail.com>
2017-10-27 11:10:12 +0200

    Xcode Project Creation

---
+++ Batelu_Luayalu/Batelu_Luayalu/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Batelu_Luayalu
+//
+//  Created by OFFROY Vincent on 27/10/2017.
+//  Copyright © 2017 OFFROY Vincent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
